cbb6168f6733d4e9dbc0d88a6867b5c706734017
1 <a1@1s-MacBook-Air.local>
2024-02-18 11:04:41 +0330

    add api slice and extre reducer

--- src/features/login/login-slice.ts
+++ src/features/login/login-slice.ts
@@ -1,33 +1,66 @@
-import type { PayloadAction } from "@reduxjs/toolkit"
-import { createAppSlice } from "../../app/createAppSlice"
-import type { AppThunk } from "../../app/store"
+import { createSlice } from '@reduxjs/toolkit'
+import type { PayloadAction } from '@reduxjs/toolkit'
 
-export interface LoginSliceInterface {
-  value: number
-  status: "idle" | "loading" | "failed"
+import type { RootState } from '../../app/store'
+import { User } from '../../app/extraApiService'
+
+type AuthState = {
+  user: User | null
+  token: string | null
 }
 
-const initialState: LoginSliceInterface = {
-  value: 0,
-  status: "idle",
-}
-
-// If you are not using async thunks you can use the standalone `createSlice`.
-export const loginSlice = createAppSlice({
-  name: "counter",
-  // `createSlice` will infer the state type from the `initialState` argument
-  initialState,
-  // The `reducers` field lets us define reducers and generate associated actions
-  reducers: create => ({
-  }),
-
-  selectors: {
-    selectCount: login => login.value,
-    selectStatus: login => login.status,
+const slice = createSlice({
+  name: 'auth',
+  initialState: { user: null, token: null } as AuthState,
+  reducers: {
+    setCredentials: (
+      state,
+      {
+        payload: { user, token },
+      }: PayloadAction<{ user: User; token: string }>,
+    ) => {
+      state.user = user
+      state.token = token
+    },
   },
 })
 
-export const { } =
-  loginSlice.actions
+export const { setCredentials } = slice.actions
 
-export const { selectCount, selectStatus } = loginSlice.selectors
+export default slice.reducer
+
+export const selectCurrentUser = (state: RootState) => state.auth.user
+
+
+// import type { PayloadAction } from "@reduxjs/toolkit"
+// import { createAppSlice } from "../../app/createAppSlice"
+
+// export interface LoginSliceInterface {
+//   value: number
+//   status: "idle" | "loading" | "failed"
+// }
+
+// const initialState: LoginSliceInterface = {
+//   value: 0,
+//   status: "idle",
+// }
+
+// // If you are not using async thunks you can use the standalone `createSlice`.
+// export const loginSlice = createAppSlice({
+//   name: "counter",
+//   // `createSlice` will infer the state type from the `initialState` argument
+//   initialState,
+//   // The `reducers` field lets us define reducers and generate associated actions
+//   reducers: create => ({
+//   }),
+
+//   selectors: {
+//     selectCount: login => login.value,
+//     selectStatus: login => login.status,
+//   },
+// })
+
+// export const { } =
+//   loginSlice.actions
+
+// export const { selectCount, selectStatus } = loginSlice.selectors
--- src/features/renew/validators.ts
+++ src/features/renew/validators.ts
@@ -20,7 +20,7 @@ export const RenewingPassValidationSchema = yup.object({
     rePassword: yup
         .string()
         .min(8, 'Password should be of minimum 8 characters length')
-        .required('Password is required')
+        .required('Password repeat is required')
         .matches(
             /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/,
             "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and One Special Case Character"
